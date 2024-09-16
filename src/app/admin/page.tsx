81618d8523dfdded14379a00d241e2c331722e4e
dipesh227 <dipeshjoshi227@gmail.com>
2024-09-16 12:57:22 +0530

    bin

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Sidebar } from "@/components/ui/sidebar";
+
+function admin() {
+    return (
+
+        <>
+            Sidebar
+        </>
+    )
+}
+
+export default admin
